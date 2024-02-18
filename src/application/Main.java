a4d6a49949fdbe3762c498f4e3ec59256ed85263
Joey Alan <joeyalan50@gmail.com>
2024-02-18 11:57:51 -0300

    initial feature

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,14 +5,28 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import java.io.File;
 
 public class Main extends Application{
 	@Override
 	public void start(Stage stage) throws Exception{
-		
+		String iconPath = "src/icons/icon_logo_eletra.png";
+
+        
+        File iconFile = new File(iconPath);
+        
+        
+        Image icon = new Image(iconFile.toURI().toString());
+        
+       
+        stage.getIcons().add(icon);
+        
 		Parent root = FXMLLoader.load(getClass().getResource("/resources/MainView.fxml"));
 
 		Scene scene = new Scene(root, 300, 275);
+		
+		 scene.getStylesheets().add(getClass().getResource("/resources/styles.css").toExternalForm());
 		
 		stage.setTitle("Projeto integração");
 		stage.setScene(scene);
--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -43,10 +43,12 @@ public class MainController implements Initializable {
         TreeItem<String> rootItem = new TreeItem<>(newValue);
 
         if ("Cronos".equals(newValue)) {
-           
+        	//CRONOS CATEGORY
             TreeItem<String> categoryItem1 = new TreeItem<>("Cronos NG");
+            TreeItem<String> categoryItem2 = new TreeItem<>("Cronos Old");
+            TreeItem<String> categoryItem3 = new TreeItem<>("Cronos L");
             
-            //CRONOS
+            //CRONOS NG
             TreeItem<String> modelItem1 = new TreeItem<>("Cronos 6001-NG");
             TreeItem<String> modelItem2 = new TreeItem<>("Cronos 6003-NG");
             TreeItem<String> modelItem3 = new TreeItem<>("Cronos 6021-NG");
@@ -54,20 +56,35 @@ public class MainController implements Initializable {
             TreeItem<String> modelItem5 = new TreeItem<>("Cronos 7021-NG");
             TreeItem<String> modelItem6 = new TreeItem<>("Cronos 7023-NG");
             
-            categoryItem1.getChildren().addAll(modelItem1, modelItem2, modelItem3, modelItem4, modelItem5, modelItem6);
+            //CRONOS Old
+            TreeItem<String> modelItem7 = new TreeItem<>("Cronos 6001-A");
+            TreeItem<String> modelItem8 = new TreeItem<>("Cronos 6003");
+            TreeItem<String> modelItem9 = new TreeItem<>("Cronos 7023");
             
-            rootItem.getChildren().addAll(categoryItem1);
+            
+            //CRONOS L
+            TreeItem<String> modelItem10 = new TreeItem<>("Cronos 6021L");
+            TreeItem<String> modelItem11 = new TreeItem<>("Cronos 6021L");
+            TreeItem<String> modelItem12 = new TreeItem<>("Cronos 7023L");
+            
+            categoryItem1.getChildren().addAll(modelItem1, modelItem2, modelItem3, modelItem4, modelItem5, modelItem6);
+            categoryItem2.getChildren().addAll(modelItem7, modelItem8, modelItem9);
+            categoryItem3.getChildren().addAll(modelItem10, modelItem11, modelItem12);
+            
+            rootItem.getChildren().addAll(categoryItem1,categoryItem2,categoryItem3);
         }
         if ("Ares".equals(newValue)) {
             
             //ARES CATEGORY
             TreeItem<String> categoryItem1 = new TreeItem<>("Ares TB");
             TreeItem<String> categoryItem2 = new TreeItem<>("Ares THS");
-            //ARES
+            
+            //ARES TB
             TreeItem<String> modelItem1 = new TreeItem<>("ARES 7021");
             TreeItem<String> modelItem2 = new TreeItem<>("ARES 7031");
             TreeItem<String> modelItem3 = new TreeItem<>("ARES 7023");
-
+            
+            //ARES THS
             TreeItem<String> modelItem4 = new TreeItem<>("ARES 8023 15");
             TreeItem<String> modelItem5 = new TreeItem<>("ARES 8023 200");
             TreeItem<String> modelItem6 = new TreeItem<>("ARES 8023 2,5");
