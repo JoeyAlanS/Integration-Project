a0567dc4763d9bd50a1c37cf3257782c6c1b8893
Joey Alan <joeyalan50@gmail.com>
2024-02-19 16:43:19 -0300

    tab correction

--- src/application/Main.java
+++ src/application/Main.java
@@ -8,28 +8,20 @@ import javafx.stage.Stage;
 import javafx.scene.image.Image;
 import java.io.File;
 
-public class Main extends Application{
-	@Override
-	public void start(Stage stage) throws Exception{
-		String iconPath = "src/icons/icon_logo_eletra.png";
-
-        
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        String iconPath = "src/icons/icon_logo_eletra.png";
         File iconFile = new File(iconPath);
-        
-        
         Image icon = new Image(iconFile.toURI().toString());
-        
-       
         stage.getIcons().add(icon);
-        
-		Parent root = FXMLLoader.load(getClass().getResource("/resources/MainView.fxml"));
 
-		Scene scene = new Scene(root, 300, 275);
-		
-		 scene.getStylesheets().add(getClass().getResource("/resources/styles.css").toExternalForm());
-		
-		stage.setTitle("Projeto integração");
-		stage.setScene(scene);
-		stage.show();
-	}
+        Parent root = FXMLLoader.load(getClass().getResource("/resources/MainView.fxml"));
+        Scene scene = new Scene(root, 300, 275);
+        scene.getStylesheets().add(getClass().getResource("/resources/styles.css").toExternalForm());
+        
+        stage.setTitle("Projeto integração");
+        stage.setScene(scene);
+        stage.show();
+    }
 }
--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,8 +1,10 @@
 package controller;
+
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.collections.FXCollections;
+
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -25,78 +27,69 @@ public class MainController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        
         comboBox.setItems(FXCollections.observableArrayList("Cronos", "Ares"));
 
-       
         comboBox.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
             if (newValue != null) {
-                
                 treeReference(newValue);
             }
         });
-        
     }
 
     public void treeReference(String newValue) {
-      
         TreeItem<String> rootItem = new TreeItem<>(newValue);
 
         if ("Cronos".equals(newValue)) {
-        	//CRONOS CATEGORY
+            // CRONOS CATEGORY
             TreeItem<String> categoryItem1 = new TreeItem<>("Cronos NG");
             TreeItem<String> categoryItem2 = new TreeItem<>("Cronos Old");
             TreeItem<String> categoryItem3 = new TreeItem<>("Cronos L");
-            
-            //CRONOS NG
+
+            // CRONOS NG
             TreeItem<String> modelItem1 = new TreeItem<>("Cronos 6001-NG");
             TreeItem<String> modelItem2 = new TreeItem<>("Cronos 6003-NG");
             TreeItem<String> modelItem3 = new TreeItem<>("Cronos 6021-NG");
             TreeItem<String> modelItem4 = new TreeItem<>("Cronos 6031-NG");
             TreeItem<String> modelItem5 = new TreeItem<>("Cronos 7021-NG");
             TreeItem<String> modelItem6 = new TreeItem<>("Cronos 7023-NG");
-            
-            //CRONOS Old
+
+            // CRONOS Old
             TreeItem<String> modelItem7 = new TreeItem<>("Cronos 6001-A");
             TreeItem<String> modelItem8 = new TreeItem<>("Cronos 6003");
             TreeItem<String> modelItem9 = new TreeItem<>("Cronos 7023");
-            
-            
-            //CRONOS L
+
+            // CRONOS L
             TreeItem<String> modelItem10 = new TreeItem<>("Cronos 6021L");
             TreeItem<String> modelItem11 = new TreeItem<>("Cronos 6021L");
             TreeItem<String> modelItem12 = new TreeItem<>("Cronos 7023L");
-            
+
             categoryItem1.getChildren().addAll(modelItem1, modelItem2, modelItem3, modelItem4, modelItem5, modelItem6);
             categoryItem2.getChildren().addAll(modelItem7, modelItem8, modelItem9);
             categoryItem3.getChildren().addAll(modelItem10, modelItem11, modelItem12);
-            
-            rootItem.getChildren().addAll(categoryItem1,categoryItem2,categoryItem3);
+
+            rootItem.getChildren().addAll(categoryItem1, categoryItem2, categoryItem3);
         }
         if ("Ares".equals(newValue)) {
-            
-            //ARES CATEGORY
+            // ARES CATEGORY
             TreeItem<String> categoryItem1 = new TreeItem<>("Ares TB");
             TreeItem<String> categoryItem2 = new TreeItem<>("Ares THS");
-            
-            //ARES TB
+
+            // ARES TB
             TreeItem<String> modelItem1 = new TreeItem<>("ARES 7021");
             TreeItem<String> modelItem2 = new TreeItem<>("ARES 7031");
             TreeItem<String> modelItem3 = new TreeItem<>("ARES 7023");
-            
-            //ARES THS
+
+            // ARES THS
             TreeItem<String> modelItem4 = new TreeItem<>("ARES 8023 15");
             TreeItem<String> modelItem5 = new TreeItem<>("ARES 8023 200");
             TreeItem<String> modelItem6 = new TreeItem<>("ARES 8023 2,5");
 
-            
             categoryItem1.getChildren().addAll(modelItem1, modelItem2, modelItem3);
-            categoryItem2.getChildren().addAll(modelItem4, modelItem5,modelItem6);         
+            categoryItem2.getChildren().addAll(modelItem4, modelItem5, modelItem6);
 
             rootItem.getChildren().addAll(categoryItem1, categoryItem2);
         }
 
-        
         treeView.setRoot(rootItem);
     }
 }
